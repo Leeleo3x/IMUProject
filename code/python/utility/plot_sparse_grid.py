cd334ecafc8cd7c66562800a739a83c5752b17db
higerra <yanhangxiong@gmail.com>
2017-11-04 23:21:09 -0500

    Refactored python/utility/plot_sparse_grid.py

--- code/python/utility/plot_sparse_grid.py
+++ code/python/utility/plot_sparse_grid.py
@@ -39,7 +39,8 @@ if __name__ == '__main__':
     ts = data_all['time'].values / 1e09
     ts -= ts[0]
 
-    window = 30
+    bias_start_id = 10
+    bias_end_id = 15
 
     linacce = gaussian_filter1d(data_all[['linacce_x', 'linacce_y', 'linacce_z']].values, sigma=30.0, axis=0)
     gravity = data_all[['grav_x', 'grav_y', 'grav_z']].values
@@ -58,35 +59,43 @@ if __name__ == '__main__':
     constraint_ind = constraint[:, 0].astype(int)
 
     font_config = {'family': 'serif',
-                   'size': 60}
-    linew = 8.0
-    markersize = 30
+                   'size': 18}
+    linew = 1.5
+    markersize = 15
 
     plt.rc('font', **font_config)
-    fig = plt.figure('Sparse grid', figsize=(60, 12))
+    fig = plt.figure('Sparse grid', figsize=(20, 5))
 
-    end_id = bias_ind[window]
+    glob_start_id = bias_ind[bias_start_id]
+    glob_end_id = bias_ind[bias_end_id]
 
-    plt.subplot(121, axis_bgcolor='black')
+    bias_end_id = min(bias_ind.shape[0] - 1, bias_end_id + 1)
+
+    # First draw the plot for low-frequency bias.
+    plt.subplot(121)
     plt.ylabel('Acceleration (m/s2)')
     plt.xlabel('Time (s)')
-    plt.plot(ts[:end_id], linacce[:end_id, 2], color='r', lw=linew)
-    # plt.plot(ts[:end_id], corrected_linacce[:end_id, 3], color='b', lw=linew)
-    # plt.plot(ts[bias_ind[:window]], bias[:window, 3], color=(.5, .8, .0), lw=linew, marker='o', markersize=markersize)
+    plt.plot(ts[glob_start_id:glob_end_id], linacce[glob_start_id:glob_end_id, 2], color='r', lw=linew)
+    plt.plot(ts[glob_start_id:glob_end_id], corrected_linacce[glob_start_id:glob_end_id, 3], color='b', lw=linew)
+    plt.plot(ts[bias_ind[bias_start_id:bias_end_id]], bias[bias_start_id:bias_end_id, 3], color=(.5, .8, .0),
+             lw=linew, marker='o', markersize=markersize)
     # plt.legend(['Sparse bias', 'Raw acceleration', 'Corrected acceleration'], loc='lower left')
 
-    cw = 0
-    for i in range(constraint_ind.shape[0]):
-        cw = i
-        if constraint_ind[i] > bias_ind[window]:
-            break
-    plt.subplot(122, axis_bgcolor='black')
+    # Then draw the plot for velocity constraints.
+    con_start_id = np.where(constraint_ind > glob_start_id)[0][0] - 1
+    con_end_id = np.where(constraint_ind > glob_end_id)[0][0] + 1
+
+    glob_start_id = constraint_ind[con_start_id]
+    glob_end_id = constraint_ind[con_end_id]
+
+    plt.subplot(122)
     plt.ylabel('Velocity (m/s)')
     plt.xlabel('Time (s)')
-    plt.plot(ts[:end_id], speed_raw[:end_id, 2], color=(0., 0., 0.), lw=linew)
-    # plt.plot(ts[:end_id], speed_corrected[:end_id, 2], color='b', lw=linew)
-    plt.plot(ts[constraint_ind[:cw]], constraint[:cw, 3], color=(.8, .5, 0), lw=linew,
-             marker='^', markersize=markersize)
+    # plt.xlim(ts[glob_start_id], ts[glob_end_id])
+    plt.plot(ts[glob_start_id:glob_end_id], speed_raw[glob_start_id:glob_end_id, 2], color=(0., 0., 0.), lw=linew)
+    plt.plot(ts[glob_start_id:glob_end_id], speed_corrected[glob_start_id:glob_end_id, 2], color='b', lw=linew)
+    plt.plot(ts[constraint_ind[con_start_id:con_end_id]], constraint[con_start_id:con_end_id, 3], color=(.8, .5, 0),
+             lw=0, marker='^', markersize=markersize)
 
     # plt.legend(['Predicted speed', 'Before correction', 'After correction'], loc='lower left')
 
