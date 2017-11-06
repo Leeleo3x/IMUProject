414125dc8e4b973ec9e67602ce6ffe1b9c80a087
higerra <yanhangxiong@gmail.com>
2017-11-05 23:51:41 -0600

    python/utility/plot_sparse_figure.py

--- code/python/utility/plot_sparse_grid.py
+++ code/python/utility/plot_sparse_grid.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import quaternion
 import argparse
@@ -39,8 +40,8 @@ if __name__ == '__main__':
     ts = data_all['time'].values / 1e09
     ts -= ts[0]
 
-    bias_start_id = 10
-    bias_end_id = 15
+    bias_start_id = 8
+    bias_end_id = 14
 
     linacce = gaussian_filter1d(data_all[['linacce_x', 'linacce_y', 'linacce_z']].values, sigma=30.0, axis=0)
     gravity = data_all[['grav_x', 'grav_y', 'grav_z']].values
@@ -59,12 +60,12 @@ if __name__ == '__main__':
     constraint_ind = constraint[:, 0].astype(int)
 
     font_config = {'family': 'serif',
-                   'size': 18}
-    linew = 1.5
-    markersize = 15
+                   'size': 80}
+    linew = 10.0
+    markersize = 70
 
     plt.rc('font', **font_config)
-    fig = plt.figure('Sparse grid', figsize=(20, 5))
+    fig = plt.figure('Sparse grid', figsize=(100, 20))
 
     glob_start_id = bias_ind[bias_start_id]
     glob_end_id = bias_ind[bias_end_id]
@@ -75,6 +76,9 @@ if __name__ == '__main__':
     plt.subplot(121)
     plt.ylabel('Acceleration (m/s2)')
     plt.xlabel('Time (s)')
+    x_min = min(ts[bias_ind[bias_start_id]], ts[glob_start_id]) - 0.1
+    x_max = max(ts[bias_ind[bias_end_id - 1]], ts[glob_end_id]) + 0.1
+    plt.xlim(x_min, x_max)
     plt.plot(ts[glob_start_id:glob_end_id], linacce[glob_start_id:glob_end_id, 2], color='r', lw=linew)
     plt.plot(ts[glob_start_id:glob_end_id], corrected_linacce[glob_start_id:glob_end_id, 3], color='b', lw=linew)
     plt.plot(ts[bias_ind[bias_start_id:bias_end_id]], bias[bias_start_id:bias_end_id, 3], color=(.5, .8, .0),
@@ -91,13 +95,15 @@ if __name__ == '__main__':
     plt.subplot(122)
     plt.ylabel('Velocity (m/s)')
     plt.xlabel('Time (s)')
-    # plt.xlim(ts[glob_start_id], ts[glob_end_id])
-    plt.plot(ts[glob_start_id:glob_end_id], speed_raw[glob_start_id:glob_end_id, 2], color=(0., 0., 0.), lw=linew)
+    x_min = min(ts[constraint_ind[con_start_id]], ts[glob_start_id]) - 0.1
+    x_max = max(ts[constraint_ind[con_end_id - 1]], ts[glob_end_id]) + 0.1
+    plt.xlim(x_min, x_max)
+    plt.plot(ts[glob_start_id:glob_end_id], speed_raw[glob_start_id:glob_end_id, 2], color='r', lw=linew)
     plt.plot(ts[glob_start_id:glob_end_id], speed_corrected[glob_start_id:glob_end_id, 2], color='b', lw=linew)
     plt.plot(ts[constraint_ind[con_start_id:con_end_id]], constraint[con_start_id:con_end_id, 3], color=(.8, .5, 0),
              lw=0, marker='^', markersize=markersize)
 
     # plt.legend(['Predicted speed', 'Before correction', 'After correction'], loc='lower left')
 
-    # fig.savefig('/Users/yanhang/Documents/research/paper/iccv2017_supplementary/presentation/alg1.png', bbox_inches='tight')
-    plt.show()
+    fig.savefig(args.dir + '/alg1.png', bbox_inches='tight')
+    # plt.show()
